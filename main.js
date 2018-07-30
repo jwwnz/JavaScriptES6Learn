6fe0bcdf7781997bf61922dc3a9f1a93c554a925
jwwnz <jameswon.nz@gmail.com>
2018-07-30 16:29:24 +1200

    Add base index html and main js script

---
+++ main.js
@@ -0,0 +1,41 @@
+// CONST & LET
+let name = "John";
+let test;
+
+name = "Jack";
+
+const person = {
+  name: "John",
+  age: 33
+};
+
+person.name = "jack";
+
+const nums = [1, 2, 3, 4];
+nums.push(5);
+
+console.log(nums);
+
+// ARROW FUNCTIONS
+
+function sayHello() {
+  console.log("hello");
+}
+
+sayHello();
+
+// FOREACH
+
+// MAP
+
+// FILTER
+
+// SPREAD
+
+// DESTRUCTURING
+
+// CLASSES
+
+// SUBCLASSES
+
+// MODULES
