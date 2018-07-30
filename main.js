f7092875e16d012a8eb135e121207bb631b1a98a
jwwnz <jameswon.nz@gmail.com>
2018-07-30 16:42:32 +1200

    filter examples

--- main.js
+++ main.js
@@ -35,7 +35,17 @@ fruits.forEach((fruit, index) => console.log(fruit + index));
 const singleFruit = fruits.map(fruit => fruit.slice(0, -1).toUpperCase());
 
 console.log(singleFruit);
+
 // FILTER
+const people = [
+  { id: 1, name: "Bryan" },
+  { id: 2, name: "Jongwoo" },
+  { id: 3, name: "Min" }
+];
+
+const people2 = people.filter(person => person.id != 2);
+
+console.log(people2);
 
 // SPREAD
 
